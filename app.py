2e6ac20a881548e09236503d8b0f950da3b28204
rhedgeco <rhedgeco@gmail.com>
2020-01-16 12:35:16 -0800

    revision to app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from general_falcon_webserver import WebApp, SqliteDatabase
+from general_falcon_webserver import WebApp
 
-app = application = WebApp('frontend','page404.html')
+app = application = WebApp('frontend', 'page404.html')
 
 app.launch_webserver()
